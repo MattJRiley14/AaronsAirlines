d3faedaa313a0db1848b1d4d0a2a0f0acb0344b3
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-01-11 09:19:47 -0600

    Initial Commit

---
+++ AaronsAirlines/AaronsAirlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AaronsAirlinesApp.swift
+//  AaronsAirlines
+//
+//  Created by Matthew RIley on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AaronsAirlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
