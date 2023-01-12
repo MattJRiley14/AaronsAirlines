30fbaac2c8418b0865c1458e29f09998d1aeb304
Matthew Riley <matthew.riley@barren.kyschool.us>
2023-01-12 08:44:58 -0600

    Completed Bonus

--- AaronsAirlines/ContentView.swift
+++ AaronsAirlines/ContentView.swift
@@ -10,9 +10,56 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Exercise Complete!")
+            Spacer()
+            
+            Text("AARON'S AIRLINES")
+                .fontWeight(.bold)
+            // OR
+            //  .bold()
+                .foregroundColor(.blue)
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            ZStack {
+                Image("airplane")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Come Fly With Us")
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+            }
+
+            Spacer()
+            
+            HStack {
+                Spacer()
+                
+                Button("Book A Flight"){
+                    print("Flight Booked")
+                }
+                .bold()
+                
+                Spacer()
+                
+                Button("Flight Status"){
+                    print("Flight is on time")
+                }
+                .bold()
+                
+                Spacer()
+                
+                Button("Check In"){
+                    print("Check in complete")
+                }
+                .bold()
+                
+                Spacer()
+            }
+            
+            Spacer()
+            
         }
-        .padding()
     }
 }
 
